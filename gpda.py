05d8cd9c20cd8c93882431f3b0c9233aa8f74a2a
Mark Robinson <mdrobinson@wpi.edu>
2021-06-18 12:57:47 -0500

    init

--- gpda.py
+++ gpda.py
@@ -1,17 +1,51 @@
-import torch
+import torch 
 import torch.nn as nn
-from torchvision import transforms
-from torchvision.models import resnet50, 
-import matplotlib.pyplot as plt
 
-import torch.optim as optim
-
-from PIL import Image
 
 class GPDA(nn.Module):
-    def __init__(self, model):
+    def __init__(self, model, Pt)
         super(GPDA, self).__init__()
+        self.model = model
+        self.Pt = Pt
 
-    def forwared(self, image):
-        x = model(image)
+        self.relu = torch.nn.ReLU()
+        self.conv1 = nn.Conv2d(16, 22, 3)
+        self.conv2 = nn.Conv2d(22, 32, 3)
+        self.conv3 = nn.Conv2d(32, 45, 3)
+        self.conv4 = nn.Conv2d(45, 64, 3)
+        self.conv5 = nn.Conv2d(64, 22, 3)
+        self.conv6 = nn.Conv2d(16, 22, 3)
+
+        self.bn1 = nn.BatchNorm2d(16)
+        self.bn2 = nn.BatchNorm2d(22)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.bn4 = nn.BatchNorm2d(45)
+        self.bn5 = nn.BatchNorm2d(64)
+        self.bn6 = nn.BatchNorm2d(3)
+
+    def forward(self, image):
+        B, N, D = image.shape
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv12(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.relu(x)
+
+        x = self.conv5(x)
+        x = self.bn5(x)
+        x = self.relu(x)
+
+        x = self.conv6(x)
+        x = self.bn6(x)
+        x = self.relu(x)
         return x
---
+++ label_target.py
@@ -0,0 +1,86 @@
+from PIL import Image
+import PIL
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from torchvision.models import resnet50
+import torch.nn as nn
+import torch.optim as optim
+import torch
+import json
+from io import BytesIO
+import numpy as np
+import random
+
+def jpegCompression(image):
+    qf = random.randrange(10, 100)
+    outputIoStream = BytesIO()
+    image.save(outputIoStream, "JPEG", quality=qf, optimice=True)
+    outputIoStream.seek(0)
+    return Image.open(outputIoStream)
+
+def normalize(input):
+  transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.244, 0.255])(input)
+  return input
+
+preprocess = transforms.Compose([
+  transforms.Lambda(jpegCompression),
+  transforms.Resize(224),
+  transforms.ToTensor(),
+  transforms.Lambda(normalize)
+])
+
+input = Image.open("examples/cat.jpg")
+input_tensor = preprocess(input)[None, :, :, :]
+
+model = resnet50(pretrained=True)
+model.eval()
+
+label = model(input_tensor).max(dim=1)[1].item()
+target = 21
+
+# targeted attacks
+epsilon = 2.0 / 255
+delta = torch.zeros_like(input_tensor, requires_grad=True)
+opt = optim.SGD([delta], lr=1e-1)
+
+toPil = transforms.ToPILImage()
+toTensor = transforms.ToTensor()
+
+ce = nn.CrossEntropyLoss()
+for t in range(100):
+  adv = input_tensor + delta
+  #adv = toTensor(jpegCompression(toPil(adv)))
+  pred = model(normalize(adv))
+  loss1 = -ce(pred, torch.LongTensor([label]))
+  loss2 = ce(pred, torch.LongTensor([target]))
+  loss = loss1 + loss2
+  if t % 10 == 0:
+    print(t, loss.item())
+  
+  opt.zero_grad()
+  loss.backward()
+  opt.step() # preform optimization step
+  delta.data.clamp(-epsilon, epsilon)
+
+ce = nn.CrossEntropyLoss()
+l2 = nn.MSELoss()
+for t in range(100):
+  adv = input_tensor + delta
+  pred = model(normalize(adv))
+  loss1 = ce(pred, torch.LongTensor([21]))
+  loss2 =  (l2(input_tensor - adv)) / (l2(input_tensor))
+  loss = loss1 + loss2
+  g = adv.grad.clone()
+  loss.backward()
+  if t % 10 == 0:
+    print(t, loss.item())
+  adv -= 1e-1 * g
+  adv.data.clamp(-epsilon, epsilon)
+
+
+with open("examples/imagenet_class_index.json") as f:
+  imagenet_classes = {int(i):x[1] for i, x in json.load(f).items()}
+
+max_class = pred.max(dim=1)[1].item()
+print("Predicted class: ", imagenet_classes[max_class])
+print("Predicted probability: ", nn.Softmax(dim=1)(pred)[0, max_class].item())
